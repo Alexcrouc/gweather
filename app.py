fc3aa668ce7d153ac67502ad6e61b7aaf891c4a7
Alex <croucao@pedarecc.sa.edu.au>
2024-06-16 15:35:32 +0930

    added comments

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    # api key is how you access the data
     api_key = 'ea68e3d3ed994ec7a7f8dc81ae326d84'
+    # get the city if not there then use adelaide
     city = request.args.get('city')
     if city is None:
         city = 'adelaide'
+    # make the request
     url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&APPID="+api_key
     print(url)
     response = requests.get(url).json()
+    # parse the response
     city_name = response["city"]["name"]
     forecast_list = response['list']
     forecast_data = []
@@ -34,43 +38,11 @@ def index():
             "desc":desc
         }
         forecast_data.append(dict)
-
         index += 8
     print(forecast_data)
-
+    # return the rendered html, passing in the data
     return render_template('home.html', forecast_data=forecast_data, city_name=city_name)
 
-
-# @app.route('/results', methods=["GET", "POST"])
-# def results():
-
-    # city = response.get('city')
-    # location = city.get('name')
-    # timezone = city.get('timezone')
-    # timestamp = city.get('dt')
-    #
-    # forecast_list = location_response["list"]
-    # timestamp = forecast_list[0]['dt']
-    # date_time = datetime.fromtimestamp(timestamp)
-    # temp = forecast_list[0]['main']['temp']
-    # description = forecast_list[0]['weather'][0]['description']
-    # wind_speed = forecast_list[0]['wind']['speed']
-    # first_time = datetime.fromtimestamp(forecast_list[0]['dt'])
-    # daily_min = list([99] * 6)
-    # daily_max = list([-99] * 6)
-    # daily_name = list([''] * 6)
-    # for forecast in forecast_list:
-    #     dt = datetime.fromtimestamp(forecast['dt'])
-    #     forecast['time'] = dt.strftime('%a %d %b, %I %p')
-    #     day_number = dt.day - first_time.day
-    #     forecast['daynumber'] = day_number
-    #     temp_min = forecast['main']['temp_min']
-    #     temp_max = forecast['main']['temp_max']
-    #     daily_min[day_number] = min(daily_min[day_number], temp_min)
-    #     daily_max[day_number] = max(daily_max[day_number], temp_max)
-    #     daily_name[day_number] = dt.strftime('%a')
-    # daily_data = zip(daily_name, daily_min, daily_max)
-    # return render_template("home.html", data = location_response, daily_data = daily_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
