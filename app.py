c94574202d0b3b150d366bda51c5f1971740d26f
Alex <croucao@pedarecc.sa.edu.au>
2024-06-16 14:33:53 +0930

    Fixed HTML

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 from flask import Flask, render_template, request
 import requests
 from datetime import datetime
-
+import math
 app = Flask(__name__)
 
 
-@app.route('/')
-def search():
-    global index
+@app.route('/', methods=['GET'])
+def index():
     api_key = 'ea68e3d3ed994ec7a7f8dc81ae326d84'
-    city = 'adelaide'
+    city = request.args.get('city')
+    if city is None:
+        city = 'adelaide'
     url = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&APPID="+api_key
     print(url)
     response = requests.get(url).json()
-    print("response:")
-    print(response)
     city_name = response["city"]["name"]
-    print(city_name)
     forecast_list = response['list']
+    forecast_data = []
     index = 0
     while index < len(forecast_list):
-        print(forecast_list[index]['dt_txt'])
-        print(forecast_list[index]['main']['temp'])
-        print(forecast_list[index]['weather'][0]['icon'])
-        print(forecast_list[index]['weather'][0]['main'])
-        index += 8
+        dt_txt = forecast_list[index]['dt_txt']
+        temp = forecast_list[index]['main']['temp']
+        icon = forecast_list[index]['weather'][0]['icon']
+        desc = forecast_list[index]['weather'][0]['description']
+        date_object = datetime.strptime(dt_txt, '%Y-%m-%d %H:%M:%S')
+        day_name = date_object.strftime('%A')
+        print(day_name)
+        dict = {
+            "dt_txt":dt_txt,
+            "day_name":day_name,
+            "temp":math.floor(int(temp)-273),
+            "icon_url":"http://openweathermap.org/img/w/" + icon + ".png",
+            "desc":desc
+        }
+        forecast_data.append(dict)
 
-    forecast_data = ""
-    return render_template('home.html')
+        index += 8
+    print(forecast_data)
+
+    return render_template('home.html', forecast_data=forecast_data, city_name=city_name)
 
 
 # @app.route('/results', methods=["GET", "POST"])
